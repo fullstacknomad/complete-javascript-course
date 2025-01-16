b32ad0d9a85a042c1fba1d5dc6a0790d272d78ea
Chandra Behera <fullstacknomad@gmail.com>
2025-01-16 20:53:49 +0530

    add: done with it

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let name = "James Bond"
+let $new= "new is a keyword so adding $"
+let _function = "we can use _before naming if it is a keyword"
+const PI=3.1415;
+let firstName = "CamelCase"
+
+console.log(name);
+console.log($new);
+console.log(_function);
+console.log(PI);
+console.log(firstName);
