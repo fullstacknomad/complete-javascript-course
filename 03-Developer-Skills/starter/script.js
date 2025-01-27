8ba527f00b08709e30196efd143ffbe6a24fab8c
Chandra Behera <fullstacknomad@gmail.com>
2025-01-27 19:51:33 +0530

    add: [functional app done]

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,63 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/*
+let x = 45;
 
+const age = birthyear => 2037 - birthyear;
+console.log(age(1998)); */
+
+//using google stackoverflow and mdn
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [
+  3,
+  -2,
+  -6,
+  -1,
+  'error',
+  9,
+  13,
+  17,
+  'error',
+  15,
+  14,
+  9,
+  5,
+]; /*
+console.log(temperatures);
+// ---------------------------------------------
+function cleanArr(arr) {
+  //make valid array
+  const validArr = arr.filter(arr => typeof arr === 'number');
+  console.log(validArr);
+  // find max and min
+  let min = Math.min(...validArr);
+  let max = Math.max(...validArr);
+  console.log(min, max);
+  console.log(max - min);
+}
+cleanArr(temperatures); */
+//----------------------------------------------
+/*
+function cleanArray(arr1, arr2) {
+  console.log(arr1, arr2);
+  let arr = arr1.concat(arr2);
+  console.log(arr);
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') continue;
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  console.log(min, max);
+  console.log(max - min);
+}
+cleanArray(temperatures, temperatures2); */
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,72 @@
 'use strict';
+/* document.querySelector('.message').textContent = 'this is a new content 🎊';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '10';
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value); */
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let scoreText = document.querySelector('.score');
+let message = document.querySelector('.message');
+let number = document.querySelector('.number');
+let highScore = 0;
+number.textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = document.querySelector('.guess').value;
+  let input = Number(guess);
+  console.log(input);
+  // when no input
+  if (!input) {
+    message.textContent = 'No input';
+  }
+  //when guess is greater
+  else if (input > secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Input is greater than the Number 🤪';
+      score = score - 1;
+      scoreText.textContent = score;
+    } else {
+      message.textContent = 'You lost the game';
+      scoreText.textContent = 0;
+    }
+  }
+  // when guess is smaller
+  else if (input < secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Input is less than the Number 🥬🍂';
+      score = score - 1;
+      scoreText.textContent = score;
+    } else {
+      message.textContent = 'You lost the game';
+      scoreText.textContent = 0;
+    }
+  }
+  // when guess is correct
+  else if (input == secretNumber) {
+    message.textContent = '🎊🎊🎊 Yu hooooo!!!! Correct Guess 🎊🎊🎊🎊';
+    //number.textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+  // when guess is not valid
+  else {
+    console.log('Enter a valid number 🛑🚫⛔');
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  scoreText.textContent = score;
+  number.textContent = '?';
+  document.querySelector('.guess').value = '';
+  message.textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  number.style.width = '15rem';
+});
